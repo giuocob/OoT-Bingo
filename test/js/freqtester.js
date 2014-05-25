59e4a95bdcb49feee71eaa79a0b4402ed53c3c9d
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2014-05-25 02:36:51 -0700

    Added sample table and results updater function

--- test/js/freqtester.js
+++ test/js/freqtester.js
@@ -1,27 +1,34 @@
 // creates a list of all of the goals
-var goals = new Array();
+var allGoals = new Array();
+// list of difficulty sets
+var difficultyGroups = new Array();
 
 for(var difficulty in bingoList) {
     var items = bingoList[difficulty];
+    var difficultyGroup = new Array();
+
     for(var i = 0; i < items.length; i++) {
         var goal = items[i];
-        goals.push(goal["name"]);
+        allGoals.push(goal["name"]);
+        difficultyGroup.push(goal["name"]);
     }
+
+    difficultyGroups[difficulty] = difficultyGroup;
 }
 
 var bingoGenerator = ootBingoGenerator;
 
 // generates a mapping of goals to frequencies
-function generateFrequencies(var numCards) {
+function generateFrequencies(numCards) {
     // creates a map of goals to their occurences
     var goalCounts = new Object();
-    for(var i in goals) {
-        goal = goals[i];
+    for(var i in allGoals) {
+        goal = allGoals[i];
         goalCounts[goal] = 0;
     }
 
     // loops through a bunch of generated cards
-    for(var i = 0; i < 100000; i++) {
+    for(var i = 0; i < numCards; i++) {
         var bingoOpts = { seed: i, mode: 'normal', lang: 'name' };
 
         var bingoBoard = bingoGenerator(bingoList, bingoOpts);
@@ -34,19 +41,58 @@ function generateFrequencies(var numCards) {
     return goalCounts;
 }
 
+function createRow(goal, frequency) {
+    var goalRow = document.createElement("tr");
 
+    var goalCol = document.createElement("td");
+    var goalText = document.createTextNode(goal);
+    goalCol.appendChild(goalText);
 
-//for(var i in goals) {
-//    var name = goals[i];
-//    var frequency = goalCounts[goal];
+    var freqCol = document.createElement("td");
+    var freqText = document.createTextNode(frequency);
+    freqCol.appendChild(freqText);
 
-//    console.log(name + ": " + frequency);
+    goalRow.appendChild(goalCol);
+    goalRow.appendChild(freqCol);
+    
+    return goalRow;
+}
 
-//    //var newElement = document.createElement('div');
-//    //newElement.innerHTML = "TEST: " + name + " " + frequency;
-//    //document.getElementById("stuff").appendChild(newElement);
-//}                                       
+function updateResults(numCards) {
+    var freqtable = document.getElementById("freqtable");
 
-console.log(goalCounts);
+    // generate cards
+    goalCounts = generateFrequencies(numCards);
+
+    // clear out the old data
+    while(freqtable.firstChild) {
+        freqtable.removeChild(freqtable.firstChild);
+    }
+
+    for(var difficulty in difficultyGroups) {
+        console.log("difficulty: " + difficulty);
+        
+        // goals of this difficulty
+        var difficultyGroup = difficultyGroups[difficulty];
+
+        console.log("goals of this difficulty: ");
+        console.log(difficultyGroup);
+        
+        // total occurences of goals in this difficulty
+        var groupFreq = 0;
+        // generate table elements for this difficulty
+        for(var i in difficultyGroup) {
+            console.log(difficultyGroup[i]);
+
+            var goal = difficultyGroup[i];
+            var frequency = goalCounts[goal];
+
+            var goalRow = createRow(goal, frequency);
+            freqtable.appendChild(goalRow);
+
+            groupFreq += frequency;
+        }
+    }
 
 }
+
