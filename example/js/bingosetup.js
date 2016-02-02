b7c2927c3fb31f7bdc935c4870b4669650f06db5
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-02-02 01:25:11 -0800

    Converted all example code from tabs to spaces

--- example/js/bingosetup.js
+++ example/js/bingosetup.js
@@ -1,5 +1,5 @@
 function bingosetup() {
-	 $('.popout').click(function() {
+     $('.popout').click(function() {
         var mode = null;
         var line = $(this).attr('id');
         var name = $(this).html();
@@ -12,61 +12,61 @@ function bingosetup() {
     });
   
     $("#bingo tr td:not(.popout), #selected td").toggle(
-		function () {
-		  $(this).addClass("greensquare");
-		},
-		function () {
-		  $(this).addClass("redsquare").removeClass("greensquare");
-		},
-		function () {
-		  $(this).removeClass("redsquare");
-		}
+        function () {
+          $(this).addClass("greensquare");
+        },
+        function () {
+          $(this).addClass("redsquare").removeClass("greensquare");
+        },
+        function () {
+          $(this).removeClass("redsquare");
+        }
     
   );
   
-	$("#row1").hover(function() { $(".row1").addClass("hover"); }, function() {	$(".row1").removeClass("hover"); });
-	$("#row2").hover(function() { $(".row2").addClass("hover"); }, function() {	$(".row2").removeClass("hover"); });
-	$("#row3").hover(function() { $(".row3").addClass("hover"); }, function() {	$(".row3").removeClass("hover"); });
-	$("#row4").hover(function() { $(".row4").addClass("hover"); }, function() {	$(".row4").removeClass("hover"); });
-	$("#row5").hover(function() { $(".row5").addClass("hover"); }, function() {	$(".row5").removeClass("hover"); });
+    $("#row1").hover(function() { $(".row1").addClass("hover"); }, function() {	$(".row1").removeClass("hover"); });
+    $("#row2").hover(function() { $(".row2").addClass("hover"); }, function() {	$(".row2").removeClass("hover"); });
+    $("#row3").hover(function() { $(".row3").addClass("hover"); }, function() {	$(".row3").removeClass("hover"); });
+    $("#row4").hover(function() { $(".row4").addClass("hover"); }, function() {	$(".row4").removeClass("hover"); });
+    $("#row5").hover(function() { $(".row5").addClass("hover"); }, function() {	$(".row5").removeClass("hover"); });
 
-	$("#col1").hover(function() { $(".col1").addClass("hover"); }, function() {	$(".col1").removeClass("hover"); });
-	$("#col2").hover(function() { $(".col2").addClass("hover"); }, function() {	$(".col2").removeClass("hover"); });
-	$("#col3").hover(function() { $(".col3").addClass("hover"); }, function() {	$(".col3").removeClass("hover"); });
-	$("#col4").hover(function() { $(".col4").addClass("hover"); }, function() {	$(".col4").removeClass("hover"); });
-	$("#col5").hover(function() { $(".col5").addClass("hover"); }, function() {	$(".col5").removeClass("hover"); });
+    $("#col1").hover(function() { $(".col1").addClass("hover"); }, function() {	$(".col1").removeClass("hover"); });
+    $("#col2").hover(function() { $(".col2").addClass("hover"); }, function() {	$(".col2").removeClass("hover"); });
+    $("#col3").hover(function() { $(".col3").addClass("hover"); }, function() {	$(".col3").removeClass("hover"); });
+    $("#col4").hover(function() { $(".col4").addClass("hover"); }, function() {	$(".col4").removeClass("hover"); });
+    $("#col5").hover(function() { $(".col5").addClass("hover"); }, function() {	$(".col5").removeClass("hover"); });
 
-	$("#tlbr").hover(function() { $(".tlbr").addClass("hover"); }, function() {	$(".tlbr").removeClass("hover"); });
-	$("#bltr").hover(function() { $(".bltr").addClass("hover"); }, function() {	$(".bltr").removeClass("hover"); });
+    $("#tlbr").hover(function() { $(".tlbr").addClass("hover"); }, function() {	$(".tlbr").removeClass("hover"); });
+    $("#bltr").hover(function() { $(".bltr").addClass("hover"); }, function() {	$(".bltr").removeClass("hover"); });
 
-	var bingoOpts = {
-		seed: getUrlParameter('seed') || Math.ceil(999999 * Math.random()).toString(),
-		mode: getUrlParameter('mode') || 'normal',
-		lang: getUrlParameter('lang') || 'name'
-	};
+    var bingoOpts = {
+        seed: getUrlParameter('seed') || Math.ceil(999999 * Math.random()).toString(),
+        mode: getUrlParameter('mode') || 'normal',
+        lang: getUrlParameter('lang') || 'name'
+    };
 
-	var prettyMode = {
-		'normal': 'Normal',
-		'short': 'Short',
-		'long': 'Long'
-	};
+    var prettyMode = {
+        'normal': 'Normal',
+        'short': 'Short',
+        'long': 'Long'
+    };
 
-	var cardType = prettyMode[bingoOpts.mode];
-	var results = $("#results");
-	results.append ("<p>OoT Bingo <strong>" + bingoList["info"].version + "</strong>&emsp;Seed: <strong>" + 
-		bingoOpts.seed + "</strong>&emsp;Card type: <strong>" + cardType + "</strong></p>");
+    var cardType = prettyMode[bingoOpts.mode];
+    var results = $("#results");
+    results.append ("<p>OoT Bingo <strong>" + bingoList["info"].version + "</strong>&emsp;Seed: <strong>" +
+        bingoOpts.seed + "</strong>&emsp;Card type: <strong>" + cardType + "</strong></p>");
 
-	
-	var bingoFunc = ootBingoGenerator;
-	
-	var bingoBoard = bingoFunc(bingoList, bingoOpts);
-	if(bingoBoard) {
-		for (i=1; i<=25; i++) {  
-			$('#slot'+i).append(bingoBoard[i].name);
-		}
-	} else {
-		alert('Card could not be generated');
-	}
+
+    var bingoFunc = ootBingoGenerator;
+
+    var bingoBoard = bingoFunc(bingoList, bingoOpts);
+    if(bingoBoard) {
+        for (i=1; i<=25; i++) {
+            $('#slot'+i).append(bingoBoard[i].name);
+        }
+    } else {
+        alert('Card could not be generated');
+    }
 }
 
 $(bingosetup);
